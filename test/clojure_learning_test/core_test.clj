6842f626b1b2463cd9eadf75cec903591c7d803c
Hugh Lee <hammerha@gmail.com>
2015-12-04 23:40:53 +0900

    Add more test code form Learn X in Y minutes

--- test/clojure_learning_test/core_test.clj
+++ test/clojure_learning_test/core_test.clj
@@ -1,7 +1,25 @@
 (ns clojure-learning-test.core-test
   (:require [clojure.test :refer :all]
             [clojure-learning-test.core :refer :all]
-            [clojure.math.numeric-tower :as math]))
+            [clojure.math.numeric-tower :as math])
+  (:import java.time.LocalDateTime
+           java.time.LocalDate))
+
+; Use "use" to get all functions from the module
+; e.g., intersection, difference
+(use 'clojure.set)
+; You can choose a subset of functions to import, too
+; (use '[clojure.set :only [intersection]])
+
+; Use 'require' to import a module
+(require 'clojure.string)
+
+; You can give a module a shorter name on "import"
+(require '[clojure.string :as str])
+
+; Use import to load a Java module
+(import java.util.Date)
+(import java.util.Calendar)
 
 ; References
 ; * Test API https://clojure.github.io/clojure/clojure.test-api.html
@@ -121,6 +139,9 @@
   (is (= '(+ 1 2) (list '+ 1 2)))
   (is (= (eval '(+ 1 2)) (+ 1 2)))
 
+  ; What is difference between Vector and List?
+  ; http://stackoverflow.com/questions/1147975/in-clojure-when-should-i-use-a-vector-over-a-list-and-the-other-way-around
+  ; Vectors - ArrayList(but not seq), Lists - LinkedList
   (is (= (class [1, 2, 3]) clojure.lang.PersistentVector)) ; Why need full name?
   (is (= (class '(1 2 3)) clojure.lang.PersistentList))
   (is (= '(1 2 3) (list 1 2 3)))
@@ -210,4 +231,131 @@
     (str "Hello " name ", args: " args))
   (is (= (hello-count "SH" 1 2 3) "Hello SH, args: (1 2 3)"))
 
-  )
+  ;;;;;;
+  ; Maps
+  ;;;;;;
+
+  (is (= (class {:a 1 :b 2 :c 3}) clojure.lang.PersistentArrayMap))
+  (is (= (class (hash-map :a 1 :b 2 :c 3)) clojure.lang.PersistentHashMap))
+
+  ; Maps can use any hashable type as a key, but usually keywords are best
+  ; Keywords are like strings with some efficiency bonuses
+  (is (= (class :a) clojure.lang.Keyword))
+
+  ; Commas are treated as whiltespace and do nothing
+  (def stringmap {"a" 1, "b" 2, "c" 3})
+  (is (= (class stringmap) clojure.lang.PersistentArrayMap))
+  (is (= (stringmap "a") 1))
+  (is (= (stringmap "d") nil))
+
+  (def keymap {:a 1 :b 2 :c 3})
+  (is (= (keymap :a) 1))
+  (is (= (:a keymap) 1))
+
+  (def newkeymap (assoc keymap :d 4))
+  (is (= newkeymap {:a 1 :b 2 :c 3 :d 4}))
+  (is (= (dissoc newkeymap :c :d) {:a 1 :b 2}))
+
+  ;;;;;;
+  ; Sets
+  ;;;;;;
+
+  (is (= (class #{1 2 3}) clojure.lang.PersistentHashSet))
+  (is (= (set [1 2 3 1 2]) #{1 2 3}))
+  (is (= (conj #{1 2 3} 4) #{1 2 3 4}))
+  (is (= (disj #{1 2 3} 3) #{1 2}))
+  (is (= (#{1 2 3} 1) 1))
+  (is (= (#{1 2 3} 4) nil))
+
+  ;;;;;;;;;;;;;;
+  ; Useful forms
+  ;;;;;;;;;;;;;;
+
+  (is (= (if false "a" "b") "b"))
+  (is (= (if false "a") nil))
+
+  ; Use let to create temporary bindings
+  (is (= (let [a 1 b 2] (> b a))))
+
+  ; Group statements together with do
+  (do
+    (def a 1)
+    (def b 2)
+    (is (< a b)))
+
+  ; Functions have an implicit do
+  (defn test-impl-do [name]
+      (def n "SH")
+      (is (= name n)))
+  (test-impl-do "SH")
+
+  ; So does let
+  (let [name "SH"]
+    (is (= name "SH"))
+    (is (not (= name "HJ"))))
+
+  ; Use the threading macros (-> and ->>)
+  (is (= (->
+           {:a 1 :b 2}
+           (assoc :c 3)
+           (dissoc :b))
+         (dissoc (assoc {:a 1 :b 2} :c 3) :b)))
+
+  (is (= (->>
+           (range 10)
+           (map inc)
+           (filter odd?)
+           (into [])) ;[1 3 5 7 9]))
+         (vec (filter odd? (map inc (range 10))))))
+
+  ;;;;;;;;;
+  ; Modules
+  ;;;;;;;;;
+
+  ; Use "use" to get all functions from the module (outside of functions)
+  ; Then we can use set operations
+  (is (= (intersection #{1 2 3} #{2 3 4}) #{2 3}))
+  (is (= (difference #{1 2 3} #{2 3 4}) #{1})) ; not #{1 4}
+
+  ; Use '/' call functions from a module
+  (is (= (clojure.string/blank? "")))
+  ; #"" denotes a regular expression literal
+  (is (= (str/replace "abcd" #"[a-b]" str/upper-case) "ABcd"))
+
+  ;;;;;;
+  ; Java
+  ;;;;;;
+
+  (is (= (instance? Date (Date.))))
+  (is (> (. (Date.) getTime) 0))
+  (is (> (.getTime (Date.)) 0))
+
+  ; doto for mutable classes
+  (is (= (instance? Date (doto (Calendar/getInstance)
+           (.set 2000 1 1 0 0 0)
+           .getTime))))
+
+  (is (> (.getTime
+           (.getTime
+             (doto (Calendar/getInstance)
+               (.set 2000 1 1 0 0 0)))) 0))
+
+  ;;;;;
+  ; STM
+  ;;;;;
+
+  ; Software Transactional Memory is the mechanism Clojure uses to handle
+  ; persistent state. There are a few constructs in Clojure that use this.
+
+  (def my-atom (atom {}))
+  (is (= (swap! my-atom assoc :a 1) {:a 1}))
+  (is (= (swap! my-atom assoc :b 2) {:a 1 :b 2}))
+  (is (= (class my-atom) clojure.lang.Atom))
+  (is (= @my-atom {:a 1 :b 2}))
+
+  (def counter (atom 0))
+  (defn inc-counter []
+    (swap! counter inc))
+  (inc-counter)
+  (is (= @counter 1))
+)
