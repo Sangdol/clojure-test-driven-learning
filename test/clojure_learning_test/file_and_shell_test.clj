41b36451de7376ae8ae141f7fdb6ffcb59b7ffde
Hugh Lee <hammerha@gmail.com>
2015-12-27 10:05:40 +0900

    Add File and Shell test

---
+++ test/clojure_learning_test/file_and_shell_test.clj
@@ -0,0 +1,12 @@
+(ns clojure-learning-test.file_and_shell_test
+  (:require [clojure.test :refer :all]
+            [clojure.java.shell :refer [sh]]))
+
+(deftest shell-test
+  (is (.contains (:out (sh "pwd")) "clojure-learning-test"))
+  )
+
+(deftest file-test
+  "http://stackoverflow.com/questions/7756909/in-clojure-1-3-how-to-read-and-write-a-file"
+  (is (.contains (slurp ".gitignore") "pom.xml"))
+  )
