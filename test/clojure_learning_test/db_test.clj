d165db71e0ffe87233e18dc07c5549e2158ea2fe
Hugh Lee <hammerha@gmail.com>
2015-12-28 01:51:46 +0900

    Rename dynamic variable

--- test/clojure_learning_test/db_test.clj
+++ test/clojure_learning_test/db_test.clj
@@ -14,20 +14,20 @@
 (def ds
   (hc/make-datasource datasource-options))
 
-(def ^:dynamic db {:datasource ds})
+(def ^:dynamic *db* {:datasource ds})
 
 (defn create-tables []
-  (jdbc/db-do-commands db
-    "
-    CREATE TABLE person (
-      id INT PRIMARY KEY AUTO_INCREMENT,
-      name VARCHAR2(30) NOT NULL,
-      age INT DEFAULT 0 NOT NULL
-    )
-    "))
+  (jdbc/db-do-commands *db*
+                       "
+                       CREATE TABLE person (
+                         id INT PRIMARY KEY AUTO_INCREMENT,
+                         name VARCHAR2(30) NOT NULL,
+                         age INT DEFAULT 0 NOT NULL
+                       )
+                       "))
 
 (defn destory-tables []
-  (jdbc/db-do-commands db "DROP ALL OBJECTS")
+  (jdbc/db-do-commands *db* "DROP ALL OBJECTS")
   )
 
 (defn prep-db [test]
@@ -40,19 +40,19 @@
 ;; http://www.lispcast.com/clojure-database-test-faster
 (defn rollback [test]
   (jdbc/with-db-transaction
-    [db db]
-    (jdbc/db-set-rollback-only! db)
-    (binding [db db]
+    [*db* *db*]
+    (jdbc/db-set-rollback-only! *db*)
+    (binding [*db* *db*]
       (test))))
 (use-fixtures :each rollback)
 
 (deftest schema-test
   (defn show-tables []
-    (jdbc/query db ["SHOW TABLES"]))
+    (jdbc/query *db* ["SHOW TABLES"]))
   (is (> (count (show-tables))) 0))
 
 (defn insert-user []
-  (jdbc/insert! db :person {:name "SH"
+  (jdbc/insert! *db* :person {:name "SH"
                               :age 33}))
 (deftest insert-test
   (is (= (insert-user) (list {(keyword "scope_identity()") 1})))
@@ -62,7 +62,7 @@
 (deftest select-test
   (insert-user)
 
-  (def users (jdbc/query db ["SELECT * FROM person"]))
+  (def users (jdbc/query *db* ["SELECT * FROM person"]))
   (is (= (count users) 1))
 
   (def first-user (first users))
