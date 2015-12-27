bf096bdc495a79529fe0155db51ae13733092c3a
Hugh Lee <hammerha@gmail.com>
2015-12-27 23:54:41 +0900

    Use connection poll correctly / Rollback after each db test

--- test/clojure_learning_test/db_test.clj
+++ test/clojure_learning_test/db_test.clj
@@ -1,3 +1,7 @@
+;;; Docs
+;;; - https://github.com/clojure/java.jdbc
+;;; - http://clojure.github.io/java.jdbc/#clojure.java.jdbc/with-db-transaction
+;;; - http://clojure-doc.org/articles/ecosystem/java_jdbc/home.html
 (ns clojure-learning-test.db-test
   (:require [clojure.test :refer :all]
             [clojure.java.jdbc :as jdbc]
@@ -10,30 +14,57 @@
 (def ds
   (hc/make-datasource datasource-options))
 
+(def ^:dynamic db {:datasource ds})
+
 (defn create-tables []
-  (jdbc/db-do-commands
-    {:datasource ds}
+  (jdbc/db-do-commands db
     "
-    CREATE TABLE test_table (
-      id INT PRIMARY KEY AUTO_INCREMENT
+    CREATE TABLE person (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR2(30) NOT NULL,
+      age INT DEFAULT 0 NOT NULL
     )
     "))
 
 (defn destory-tables []
-  (jdbc/db-do-commands
-    {:datasource ds}
-    "DROP ALL OBJECTS")
+  (jdbc/db-do-commands db "DROP ALL OBJECTS")
   )
 
-(defn prep-db [f]
+(defn prep-db [test]
   (create-tables)
-  (f)
+  (test)
   (destory-tables))
 
 (use-fixtures :once prep-db)
 
-(deftest schema-test
-  (is (> (count (jdbc/with-db-connection
-           [conn {:datasource ds}]
-           (jdbc/query conn ["SHOW TABLES"]))) 0)))
+;; http://www.lispcast.com/clojure-database-test-faster
+(defn rollback [test]
+  (jdbc/with-db-transaction
+    [db db]
+    (jdbc/db-set-rollback-only! db)
+    (binding [db db]
+      (test))))
+(use-fixtures :each rollback)
 
+(deftest schema-test
+  (defn show-tables []
+    (jdbc/query db ["SHOW TABLES"]))
+  (is (> (count (show-tables))) 0))
+
+(defn insert-user []
+  (jdbc/insert! db :person {:name "SH"
+                              :age 33}))
+(deftest insert-test
+  (is (= (insert-user) (list {(keyword "scope_identity()") 1})))
+  (is (= (first (insert-user)) {(keyword "scope_identity()") 2}))
+  )
+
+(deftest select-test
+  (insert-user)
+
+  (def users (jdbc/query db ["SELECT * FROM person"]))
+  (is (= (count users) 1))
+
+  (def first-user (first users))
+  (is (= (:name first-user) "SH"))
+  (is (= (:age first-user) 33)))
