1e23b77087c06f336247fc2fd295df9c68a50a60
Hugh Lee <hammerha@gmail.com>
2016-01-10 20:17:47 +0900

    Add ring-defaults middleware tests

---
+++ test/clojure_learning_test/ring_middleware_test.clj
@@ -0,0 +1,42 @@
+;;; Ring Defaults - https://github.com/ring-clojure/ring-defaults
+(ns clojure-learning-test.ring-middleware-test
+  (:require [clojure.test :refer :all]
+            [ring.middleware.defaults :refer :all]
+            [ring.mock.request :refer :all]
+            [ring.util.response :as response]
+            [compojure.core :refer :all]
+            [compojure.route :as route]))
+
+(defroutes handler
+           (GET "/" [] "Hello world!")
+           (POST "/" [] (response/redirect-after-post "/")))
+
+(def anti-forgery-site
+  (wrap-defaults
+    handler
+    site-defaults))
+
+;; TODO Set CSRF Header Token
+;; http://stackoverflow.com/questions/20430281/set-ring-anti-forgery-csrf-header-token
+(deftest anti-forgery-site-test
+  (let [res (anti-forgery-site (request :post "/" {:name "sd"}))]
+    (is (= 403 (:status res)))
+    (is (= "<h1>Invalid anti-forgery token</h1>" (:body res)))
+    )
+  )
+
+(def site
+  (wrap-defaults
+    handler
+    (assoc-in site-defaults [:security :anti-forgery] false)))
+
+(deftest site-test
+  (let [res (site (request :get "/"))]
+    (is (= 200 (:status res)))
+    (is (= "text/html; charset=utf-8" (get-in res [:headers "Content-Type"])))
+    (is (= "Hello world!" (:body res))))
+
+  (let [res (site (request :post "/" {:name "sd"}))]
+    (is (= 303 (:status res)))
+    )
+  )
