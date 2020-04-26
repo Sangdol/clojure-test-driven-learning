3c92ecc5791f46a415a96fe43cc2f5e2d5f4feeb
Sanghyun Lee <hammerha@gmail.com>
2020-04-26 23:59:16 +0200

    Upgrade Clojure / Add lein-test-refresh

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring-defaults "0.1.5"]
                  [com.cemerick/friend "0.2.1"]
                  [ring "1.4.0"]
@@ -17,6 +17,8 @@
                  [org.clojure/math.numeric-tower "0.0.4"]]
   :main ^:skip-aot clojure-learning-test.core
   :target-path "target/%s"
-  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring/ring-mock "0.3.0"]]}})
+  :profiles {:dev
+             {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
+              :dependencies [[javax.servlet/servlet-api "2.5"]
+                             [ring/ring-mock "0.3.0"]]} })
 
