93f652ef78ac1e3828c63590f87b9a45d8168b55
Sanghyun Lee <hammerha@gmail.com>
2020-05-01 22:26:14 +0200

    Add more macro tests

--- test/clojure_learning_test/macro_test.clj
+++ test/clojure_learning_test/macro_test.clj
@@ -27,12 +27,27 @@
 (deftest syntax-quote-test
   (testing "backtick fully qualifies"
     (is (= '(clojure.core/first []) `(first []))))
-  (testing "x doesn't resolve by default"
+  (testing "x isn't resolved by default"
     (is (= `(first [x 1 2])
           (let [x 0]
             `(first [x 1 2])))))
-  (testing "tilde makes x resolve"
+  (testing "tilde makes x resolved"
     (is (= `(first [0 1 2])
            (let [x 0]
              `(first [~x 1 2]))))))
+
+
+(defmacro f [exp]
+  (let [[a op b] exp]
+    (list op a b)))
+
+(defmacro f2 [exp]
+  (let [[a op b] exp]
+    `(~op ~a ~b)))
+
+(deftest macro-op-test
+  (is (= 3 (f (1 + 2))))
+  (is (= 2 (f (1 * 2))))
+  (is (= 2 (f2 (1 * 2)))))
+
 
