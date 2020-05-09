e5ff2a4daa53011a146642a9b281343a54b2d72d
Sanghyun Lee <hammerha@gmail.com>
2020-05-09 11:25:02 +0200

    Add more clojurescript tests

---
+++ test/clojurescript_learning_test/js_test.cljs
@@ -0,0 +1,20 @@
+#_ ("
+JS namespace
+https://cljs.github.io/api/syntax/js-namespace
+")
+
+(ns clojurescript-learning-test.js-test
+  (:require [cljs.test :refer-macros [deftest is testing]]))
+
+(deftest js-object-test
+  (is (= 3.5 (.parseFloat js/window "3.5")))
+  (is (= "ABC" (.toUpperCase "abc")))
+  (is (= "ABC" (.substring "0ABC" 1 5)))
+  (is (= 3 (.-length "ABC")))
+
+  ;; Using underlying JS namespace
+  (is (= 3.5 (js/Window.parseFloat "3.5"))))
+
+(deftest creating-object-test
+  ;; (new Date).getHours()
+  (is (<= 0 (.getHours (js/Date.)))))
--- test/clojurescript_learning_test/math_test.cljs
+++ test/clojurescript_learning_test/math_test.cljs
@@ -1,5 +1,9 @@
 (ns clojurescript-learning-test.math-test
-  (:require [cljs.test :refer-macros [deftest is testing run-tests]]))
+  (:require [cljs.test :refer-macros [deftest is testing]]))
 
 (deftest math-test
-  (is (= 3.141592653589793 (.-PI js/Math))))
+  (is (= 3.141592653589793 (.-PI js/Math)))
+  ;; Math pseudo namesapce https://cljs.github.io/api/syntax/Math-namespace
+  (is (= 3.141592653589793 Math/PI))
+  (is (= 1 (.sin js/Math (/ (.-PI js/Math) 2))))
+  (is (= 1 (Math/sin (/ (.-PI js/Math) 2)))))
