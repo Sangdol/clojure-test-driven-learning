7cd83cb685c8e3889e5f82f8342076fd114fc995
Sanghyun Lee <hammerha@gmail.com>
2020-08-20 23:24:18 +0200

    Add more macro tests

--- test/clojure_learning_test/macro_test.clj
+++ test/clojure_learning_test/macro_test.clj
@@ -1,5 +1,14 @@
 (ns clojure-learning-test.macro-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [clojure.walk :as walk]))
+
+
+(deftest eval-test
+  ; take a list and execute it
+  (is (= 3 (eval '(+ 1 2))))
+  (is (= 3 (eval (list + 1 2))))
+  (let [x [1 2]]
+    (is (= 5 (eval `(+ ~@(map inc x) 0))))))
 
 
 (deftest macroexpand-test
@@ -7,8 +16,45 @@
     (is (= '(if 1 (do 2 3)) (macroexpand-1 '(when 1 2 3))))
     (is (= '(if 1 (do 2 3)) (macroexpand '(when 1 2 3))))))
 
+(defmacro return-if [& clauses]
+  (when (seq clauses)
+    (list 'if (first clauses)
+          (second clauses)
+          (cons 'return-if (nnext clauses)))))
+
+
+(defmacro do-until [& clauses]
+  (when (seq clauses)
+    (list 'clojure.core/when (first clauses)
+          (second clauses)
+          (cons 'do-until (nnext clauses)))))
+
+
+(deftest macroexpand-test2
+  (is (= 1 (return-if true 1)))
+  (is (= 3 (return-if false 2 true 3)))
+
+  ; be wary of the context
+  (is (= '(return-if true 1)
+         (macroexpand-1 '(return-if true 1))))
+
+  (is (= '(if true 1 (return-if))
+         (macroexpand-1 '(clojure-learning-test.macro-test/return-if true 1))))
+
+  (is (= '(clojure.core/when true 1 (do-until))
+         (macroexpand-1 '(clojure-learning-test.macro-test/do-until true 1))))
+
+  (is (= '(if true (do 1 (do-until)))
+         (macroexpand '(clojure-learning-test.macro-test/do-until true 1))))
+
+  ; this will expand subforms as well (what are subforms?).
+  (is (= '(if true (do 1 (do-until)))
+         (walk/macroexpand-all '(clojure-learning-test.macro-test/do-until true 1)))))
+
+
 (defn hello [target]
   (str "Hello, " target "!"))
+
 
 (defmacro def-hello [name target]
   (list 'defn
@@ -16,8 +62,10 @@
         []
         (list 'hello target)))
 
+
 (def-hello hello-sang "Sang")
 (def-hello hello-hj "HJ")
+
 
 (deftest defmacro-test
   (is (= "Hello, Sang!" (hello-sang)))
@@ -27,23 +75,47 @@
 (deftest syntax-quote-test
   (testing "backtick fully qualifies"
     (is (= '(clojure.core/first []) `(first []))))
+
   (testing "x isn't resolved by default"
     (is (= `(first [x 1 2])
           (let [x 0]
             `(first [x 1 2])))))
-  (testing "tilde makes x resolved"
+
+  (testing "tilde makes x resolved (unquote)"
     (is (= `(first [0 1 2])
            (let [x 0]
-             `(first [~x 1 2]))))))
+             `(first [~x 1 2])))))
+
+  ;; quote unquote crazy
+  (let [x '(- x)]
+    (is (= 'x 'x))
+    (is (= '(- x) x))
+    (is (= 'clojure-learning-test.macro-test/x `x))
+    (is (= (quote 'clojure-learning-test.macro-test/x) ``x))
+    (is (= (quote 'clojure-learning-test.macro-test/x) '`x))
+    (is (= (quote 'clojure-learning-test.macro-test/x) `'x))
+    (is (= (list 'quote 'clojure-learning-test.macro-test/x) `'x))
+    (is (= (quote (clojure.core/unquote x)) '~x))
+    (is (= '(- x) `~x))
+    (is (= 'clojure-learning-test.macro-test/x ``~x))
+    (is (= (quote '(- x)) `'~x)))
+
+  ;; unquote-splice
+  (let [x `(2 3)]
+    (is (= '(1 (2 3)) `(1 ~x)))
+    (is (= '(1 2 3) `(1 ~@x)))
+    (is (= '(1 3 4) `(1 ~@(map inc [2 3]))))))
 
 
 (defmacro f [exp]
   (let [[a op b] exp]
     (list op a b)))
 
+
 (defmacro f2 [exp]
   (let [[a op b] exp]
     `(~op ~a ~b)))
+
 
 (deftest macro-op-test
   (is (= 3 (f (1 + 2))))
